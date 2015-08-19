23262d0b990b449a386979f35883b54f169659d6
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-19 17:46:48 -0400

    got list items to append, and on error will display message. need to figure out why <div> append items not working

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   sortList();
   crossOff();
   showDelete();
+  addEntry();
+  deleteEntry();
 });
 
 function sortList() {
@@ -24,4 +26,38 @@ $("li").mouseenter(function(){
   $(this).children('.delete').hide();
 });
 }
+
+// function itemEntry() {
+//   $('form').submit(function() {
+//     if ($('input').val() !== '') {
+//       var input_value = $('input').val();
+//       $('.table-list ul').append('<li>' + input_value + '</li>');
+// }
+// });
+// }
+
+function addEntry() {
+  $('form').submit(function () {
+    if ($('input#items-todo').val().length == '0') {
+        $('#error').show();
+        $('input#items-todo').val("");
+        return false
+      } else {
+        $('#error').hide();
+        $('ul').append('<li>' + '<div class="checked"></div>' +  $('input#items-todo').val() + '<div class="delete"></div>' + '<hr>' + '</li>');
+    $('input#items-todo').val("");
+    return false
+    }
+  });
+}
+
+function deleteEntry() {
+  $('.delete').click(function () {
+    $(this).parent('li').remove();
+  });
+}
+
+
+
+
 
